dc4e94bd31027439fd702923ee5010bdaaae0f54
TejasGShinde <ptejas387@gmail.com>
2025-04-02 22:32:44 +0530

    StopWatch added  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useRef, useEffect } from 'react'
+import { useState,useRef, useEffect, use } from 'react'
  
  import './App.css'
  
@@ -48,15 +48,16 @@ function App() {
      }
      <button onClick={(e)=>submit(e)}>Submit</button>
      <Progress />
+     <Stopwatch />
     </>
   )
 }
 function Progress() {
-  const [progress, setProgress] = useState(60); // Initial progress (30%)
+  const [progress, setProgress] = useState(60);  
   const handleChange = (e) => {
     let value = Number(e.target.value);
-    if (value < 0) value = 0; // Prevent negative values
-    if (value > 100) value = 100; // Limit progress to 100%
+    if (value < 0) value = 0;  
+    if (value > 100) value = 100;  
     setProgress(value);
   };
   return (
@@ -67,6 +68,84 @@ function Progress() {
     </div>
     </>
   );
+}
+
+function Stopwatch(){
+    const [start,setStart]  = useState(0);
+    const [min,setMin] = useState(10);
+    const [stop,setStop] = useState(true)
+    const [timer,setTimer] = useState();
+    const [pauses,setPauses] = useState([])
+    const [isCountDown,setIsCountDown] = useState(true)
+    const handleChange = (e)=>{
+      
+      setMin(e.target.value)
+      pause()
+      setStart(0)
+    }
+    const format = (start)=>{
+          let sec = Math.floor(start/1000);
+          let mi =Math.floor( sec/60);
+          let hr = Math.floor(min/60);
+          let ms = (start%1000)
+          sec = sec%60
+          return `${hr.toString().slice(0,3).padStart(2,0)}:${mi.toString().padStart(2,0)}:${sec.toString().padStart(2,0)}:${ms.toString().padStart(3,0)}`
+    }
+    const startWatch = (e)=>{
+      console.log((start/(1000*60)))
+     setStop(false);
+     
+       
+    }
+    const pause = (e)=>{
+      setPauses((prev)=>[...prev,start])
+       setStop(true);
+      clearInterval(timer)
+    }
+    useEffect(()=>{
+     
+      if(!stop){
+       
+        const timer =  setInterval(() => {
+          setStart(prev=>{
+            if(isCountDown && Math.floor(min*60*1000 <= prev+100)){
+              console.log(min*60*1000+" "+prev)
+             clearInterval(timer)
+             alert(`${min} minutes completed`)
+              pause()
+              return prev;
+            }
+            // return prev+100;
+             return prev+10
+          });
+         
+        }, 10);
+        setTimer(timer)
+       }
+      return () => clearInterval(timer);//always should be a fucntion 
+    
+    },[stop])
+    return(
+      <>
+      <div>
+        <input type="number"  value={min} onChange={handleChange}/>
+        <input value={isCountDown} checked={isCountDown} type="checkbox" onChange={()=>setIsCountDown(prev=>!prev)}/>
+        <h1>{format(start)}</h1>
+         
+        <h1>hh:mm:ss:ms</h1>
+        <h3>{min}</h3>
+        <button onClick={startWatch}>start</button>
+        <button onClick={pause}>pause</button>
+        <button onClick={()=>setStart(0)}>Reset </button>
+        {
+          pauses && 
+          pauses.map((item,index)=>{
+            return <h3 key={index}>Pause Number:{index+1}--{format(item)}</h3>
+          })
+        }
+      </div>
+      </>
+    )
 }
 
 export default App
